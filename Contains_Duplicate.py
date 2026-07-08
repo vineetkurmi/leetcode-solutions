c97e807d346e7d4c5bfb5f8357a82e47acf0cf97
Vineet kurmi <vineetkurmi15@gmail.com>
2026-07-09 00:00:23 +0530

    Add class Solution with containsDuplicate method

---
+++ Contains_Duplicate.py
@@ -0,0 +1,14 @@
+# Que. :- 217. Contains Duplicate
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+
+            if num in seen:
+                return True
+
+            seen.add(num)
+
+        return False
