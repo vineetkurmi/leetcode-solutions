c3a243b647e256f47b5aab32be373a62fefb073b
Vineet kurmi <vineetkurmi15@gmail.com>
2026-07-17 00:00:33 +0530

    Add firstMissingPositive method in Solution class
    Implement a solution to find the first missing positive integer in a list.

---
+++ 41.py
@@ -0,0 +1,21 @@
+# Que :- 41. First Missing Positive
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] <= 0 or nums[i] > n:
+                nums[i] = n + 1
+
+        for num in nums:
+            num = abs(num)
+
+            if 1 <= num <= n:
+                nums[num - 1] = -abs(nums[num - 1])
+
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+
+        return n + 1
