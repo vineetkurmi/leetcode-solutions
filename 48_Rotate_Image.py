1b2b50bbf3d6a317ed351a84213fdf42e6a01534
Vineet kurmi <vineetkurmi15@gmail.com>
2026-07-20 23:30:30 +0530

    Create 48_Rotate_Image.py

---
+++ 48_Rotate_Image.py
@@ -0,0 +1,12 @@
+# Que. :- 48. Rotate Image
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i+1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for row in matrix:
+            row.reverse()
